5d0722bc279299731611fb490a256ba9795d7d5e
GoNz0 | Mn0g1as <128939384+Mn0g1as@users.noreply.github.com>
2023-07-08 18:57:35 +0200

    Update Youtube.py

--- Youtube.py
+++ Youtube.py
@@ -1,1 +1,11 @@
+from pytube import Youtube
+
+# Paste your Youtube link :)
+link = "https://youtu.be/paste-your-youtube-link"
+yt = Youtube(link)
+stream = yt.streams.get_highest_resolution
+
+print("Downloading. Please wait...")
+stream.download()
+print("Download completed.")
 
